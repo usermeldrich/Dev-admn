c5cc342b1bbe007d10a0a6410abffbce6d9cb1c0
Dark97B <jeffersonolivernuke05@gmail.com>
2025-05-22 23:34:57 -0300

    atualização

---
+++ painel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Título da página</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <button type="button">Lista de usuarios</button>
+
+    <a href="DEV-ADMN/usuarios_pontos.php">Visit W3Schools</a>
+  </body>
+
+</html>
