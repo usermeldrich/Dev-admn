349c9a9739533d98f51b87546ff00a5738d1a547
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-05-24 15:57:25 -0300

    estrutura inicial criada, e banco conectado

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'reserva');
 define('DB_USER', 'postgres');
 define('DB_PASS', '159357');
+
 try {
     $pdo = new PDO("pgsql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
