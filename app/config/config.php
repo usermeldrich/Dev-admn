9a3016f09de7f38436ed75a990b0838fdc578c97
usermeldrich <tomaxhereandnow@gmail.com>
2025-05-28 07:56:45 -0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'reserva');
 define('DB_USER', 'postgres');
-define('DB_PASS', '159357');
+define('DB_PASS', '*1052sonicX');
 
 try {
     $pdo = new PDO("pgsql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
