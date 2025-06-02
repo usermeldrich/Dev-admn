b59b1f2f825a7d158942cab3e96326dbcd2e7888
usermeldrich <tomaxhereandnow@gmail.com>
2025-06-02 07:53:53 -0300

    coisas novas

--- app/views/login.php
+++ app/views/login.php
@@ -3,10 +3,9 @@ require_once __DIR__ . '/../config/config.php';
 
 // Processamento do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    require_once __DIR__ . '/../config/config.php';
     session_start();
 
-    $identificador = $_POST['identificador']; // Pode ser email ou usuário
+    $identificador = $_POST['identificador'];
     $senha = $_POST['senha'];
 
     $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE (email = :id OR usuario = :id)");
@@ -22,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['usuario_usuario'] = $usuario['usuario'];
         $_SESSION['usuario_email'] = $usuario['email'];
 
-        header('Location: /Dev-admn/public/painel');
+        header('Location: ' . BASE_URL . '/painel');
         exit;
     } else {
         $erro = "Usuário/email ou senha incorretos!";
@@ -30,75 +29,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <style>
-        body {
-            background-color: #f0f4f8;
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .login-container {
-            background-color: #fff;
-            padding: 40px;
-            border-radius: 12px;
-            box-shadow: 0 8px 20px rgba(0,0,0,0.1);
-            width: 400px;
-            text-align: center;
-        }
-
-        h2 {
-            color: #333;
-            margin-bottom: 20px;
-        }
-
-        input[type="text"], input[type="password"] {
-            width: 90%;
-            padding: 12px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 8px;
-        }
-
-        button {
-            background-color: #007BFF;
-            color: white;
-            padding: 12px 25px;
-            border: none;
-            border-radius: 8px;
-            cursor: pointer;
-            margin-top: 10px;
-        }
-
-        button:hover {
-            background-color: #0056b3;
-        }
-
-        .error {
-            color: red;
-            margin-top: 10px;
-        }
-
-        .link-cadastro {
-    text-decoration: none;
-    color: #007BFF;
-}
-
-.link-cadastro:hover {
-    text-decoration: underline;
-    color: #0056b3;
-}
-
-    </style>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/styles.css">
 </head>
 <body>
 
@@ -116,8 +52,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
 
     <a class="link-cadastro" href="<?= BASE_URL ?>/cadastro">Primeira Vez Aqui? Cadastre-se</a>
-
-
 </div>
 
 </body>
